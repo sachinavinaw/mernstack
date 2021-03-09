207ce6022e78535ab8b0a2a87384f2af190a0a15
Sachin Avinaw <sachinavinaw@gmail.com>
2021-03-09 22:37:55 +0900

    updated index.js

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+axios.defaults.baseURL = "http://127.0.0.1:5000";
+axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("token");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
